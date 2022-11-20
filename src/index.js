e2d78a48f33b7e55db3618e88f080bb4106ede39
Chris <78698227+cjchanx@users.noreply.github.com>
2022-11-20 07:28:55 -0700

    Add parsing for DB data

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ const FIREBASE_DB_CONFIG = {
 var fbApp = firebase.initializeApp(FIREBASE_DB_CONFIG);
 var db = fbApp.firestore();
 
-export { db };
+export { db, FIREBASE_DB_CONFIG };
+
+var gbLocs = "";
+
+export { gbLocs };
 
 const fetch = async () => {
   const req = await db.collection('waterstore').get();
@@ -31,6 +35,30 @@ const fetch = async () => {
 
   for(let i = 0; i < tempLocs.length; i++){
     console.log('Location:', tempLocs[i].lat, tempLocs[i].long);
+  }
+
+  gbLocs = "";
+
+  for(let i = 0; i < tempLocs.length; i++) {
+    // Determine the correct icon for the marker
+    var totalpts = tempLocs[i].totalpoints;
+    var totalreviews = tempLocs[i].totalreviews;
+    totalpts = totalpts / totalreviews; // average rating
+
+    var icoType = "goodWater"; // TODO: Change default to 'neutral / grey-water'
+
+    // Calculate the rating based on conditionals
+    if(totalpts = 0 || totalpts == 2.5)
+      icoType = "goodWater";
+    else if(totalpts < 2.5)
+      icoType = "badWater";
+    else if(totalpts > 2.5)
+      icoType = "goodWater";
+
+      console.log("<MarkerF position={{ lat: " + tempLocs[i].lat + ", lng: " +  tempLocs[i].long + "}} icon={" + icoType + "}/>\n");
+
+    // Add the marker to the list
+    gbLocs += "<MarkerF position={{ lat: " + tempLocs[i].lat + ", lng: " +  tempLocs[i].long + "}} icon={" + icoType + "}/>\n";
   }
 }
 
